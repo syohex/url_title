d013c525153d0ac2699ded6e0a364b2212905109
Syohei YOSHIDA <syohex@gmail.com>
2014-12-24 12:44:13 +0900

    init repos

---
+++ README.md
@@ -0,0 +1,3 @@
+# url_title
+
+Show title of the URL and its shorten URL with [Google API](https://goo.gl/).
---
+++ url_title.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"regexp"
+
+	"github.com/typester/go-pit"
+)
+
+var titleRe = regexp.MustCompile(`(?i)<title[^>]*>([^<]+)</title>`)
+
+func getTitle(url string) (string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	matches := titleRe.FindStringSubmatch(string(content))
+	if matches == nil {
+		return "", fmt.Errorf("can't retrieve page title\n")
+	}
+
+	return matches[1], nil
+}
+
+func postedJSON(key string, url string) string {
+	return fmt.Sprintf(`{"key":"%s", "longUrl": "%s"}`, key, url)
+}
+
+const apiURL = "https://www.googleapis.com/urlshortener/v1/url"
+
+type shortenURLResponse struct {
+	ID string `json:"id"`
+}
+
+func decodeAPIResponse(r io.Reader) (string, error) {
+	decoder := json.NewDecoder(r)
+
+	var res shortenURLResponse
+	if err := decoder.Decode(&res); err != nil {
+		return "", err
+	}
+
+	return res.ID, nil
+}
+
+func shortURL(url string) (string, error) {
+	profile, err := pit.Get("goo.gl", pit.Requires{
+		"key": "API key",
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	jsonStr := postedJSON((*profile)["key"], url)
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer([]byte(jsonStr)))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	return decodeAPIResponse(res.Body)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: url_title url\n")
+		return
+	}
+
+	url := os.Args[1]
+
+	title, err := getTitle(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	shorten, err := shortURL(url)
+	fmt.Printf("%s %s", title, shorten)
+}
